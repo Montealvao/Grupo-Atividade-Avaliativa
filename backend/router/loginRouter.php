a6195a540cb105f3488d12a9e3f26df3c5de3bfb
Pedro Celestino <pedrojuniorcelestinocg@gmail.com>
2025-01-20 16:21:09 -0400

    login correto (#3)

--- backend/router/loginRouter.php
+++ backend/router/loginRouter.php
@@ -2,11 +2,10 @@
 
 include __DIR__ . "/../controller/loginController.php";
 
+session_start();
+
 $loginController = new LoginController();
 
-if (!isset($_SESSION["id_usuario"])){
-    header("location: ../../src/pages/login/login.php");
-}
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     // if(isset($routes[$_GET["action"]])) {
@@ -17,15 +16,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         case 'validarLogin':
             echo "FUNCINA";
             $resultado = $loginController->ValidarLogin($_POST["email"], $_POST["senha"]);
+
             if ($resultado){
-                header("Location: ../../src/pages/home/index.php");
+                header("Location: ../../src/pages/agendar/agendar.php");
             }
             else{
                 header("location: ../../src/pages/login/login.php");
             }
-            
             break;
-        
+
+        case  'validarsessao':
+            if (!isset($_SESSION["id_usuario"])){
+                header("location: ../../src/pages/login/login.php");
+            }
+            else {
+                header("location: ../../src/pages/agendar/agendar.php");
+            } 
+            break;
         default:
             echo "<h1>Not found 404</h1>";
             break;
