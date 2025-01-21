a305172026102fff73a004df46ca102cea20d2fe
gcosta <gcosta@grupoimagetech.com.br>
2025-01-21 17:11:52 -0400

    feat: dependencias removidas, talwind pelo cdn

--- src/pages/home/index.php
+++ src/pages/home/index.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
 <body>
     <h1>HOME</h1>
     <form action="../agendar/agendar.php">
-        <p class="bg-slate-600">ir página agendar</p>
+        <p>ir página agendar</p>
         <button type="submit">IR</button>
     </form>
 </body>
