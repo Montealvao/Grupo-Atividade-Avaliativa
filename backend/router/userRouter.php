2bddbcfeaa2ce130aab967d90df2570d19a4d92b
Guilherme Montealvão <guilhermehmc06@gmail.com>
2025-01-22 17:08:33 -0400

    feat: função de criar usuário funcionando

--- backend/router/userRouter.php
+++ backend/router/userRouter.php
@@ -1,18 +1,16 @@
 <?php
 
-require __DIR__ . "/../controller/userController.php";
-
+include __DIR__ . "/../controller/userController.php";
 
 $userController = new userController();
 
 if($_SERVER['REQUEST_METHOD'] == "POST") {
     switch ($_GET["acao"]) {
         case 'cadastrar':
-            if(!(empty($_POST['nome']) || empty($_POST['email']) || empty($_POST['senha']) empty($_POST['telefone']))){
+            if(!(empty($_POST['nome']) || empty($_POST['email']) || empty($_POST['senha']) || empty($_POST['telefone']))){
                 $resultado = $userController->CriarUsuario($_POST["nome"],$_POST["email"],$_POST["senha"],$_POST["telefone"]);
-                echo $resultado;
                 if($resultado){
-                    header("location: ../../src/pages/home/index.php");
+                    header("location: ../../src/pages/index.php");
                 }else{
                     header("location: ../../src/pages/cadastro/index.php?error=true");
                 }
@@ -22,7 +20,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
             break;
         
         case "edit":
-            if(!(empty($_POST['nome']) || empty($_POST['email']) || empty($_POST['senha']) empty($_POST['telefone']))){
+            if(!(empty($_POST['nome']) || empty($_POST['email']) || empty($_POST['senha']) || empty($_POST['telefone']))){
                 $resultado = $userController->AtualizarUsuario($_POST["id"], $_POST["nome"],$_POST["email"],$_POST["senha"],$_POST["telefone"]);
                 if($resultado){
                     header("location: ../../pages/home/index.php");
