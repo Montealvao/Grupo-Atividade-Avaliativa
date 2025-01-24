ea02f1ec7e3191f4ca3c082dd5358355e9031ee5
Samuel de Barros Sousa <mkcsamu@gmail.com>
2025-01-24 18:04:43 -0300

    começo do agendar

--- src/pages/agendar/agendar.php
+++ src/pages/agendar/agendar.php
@@ -3,14 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./agendar.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>AGENDAR PAGINA</h1>
-    <form action="../../../backend/router/loginRouter.php?action=validarsessao" method="POST">
-        <p>agendar horario</p>
-        <button>AGENDAR BUTAO</button>
-    </form>
+    <div class="container_1 ">
     
+    <div class="opcoes_1">
+
+        <strong><p>Home</p></strong>
+        <p>Explorar</p>
+        <p>sobre</p>
+
+    </div>
+
+    <input type="text" placeholder="  Pesquisar" id="pesquisa">   
+
+    <div class="Usuario">
+        <img src="https://cdn-images.dzcdn.net/images/artist/427f1c47b0e448e77172ef64e9363ad3/500x500.jpg" alt="">
+    </div>
+    </div>
+    <div id="imgReserva">
+        <p>Reserva de casa de festas FAST-FOOD</p>
+        <img id="mc" src="https://www.arcosdorados.com/wp-content/uploads/2023/11/Novo-McDonalds-Montes-Claros.png" alt="">
+    </div>
+    <div id="therock">
+        
+    </div>
 </body>
 </html> 
+<!-- <form action="../../../backend/router/loginRouter.php?action=validarsessao" method="POST">
+    <p>agendar horario</p>
+    <button>AGENDAR BUTAO</button>
+</form> -->
