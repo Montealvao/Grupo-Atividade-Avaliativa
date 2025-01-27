e21064f0980374779977250e58ee726783febaf5
Guilherme Costa <guilhermehmc06@gmail.com>
2025-01-26 22:12:22 -0400

    Feat/perfil (#8)
    * feat: desenvolvimento da page perfil e algumas funções e ajustes
    
    * função de atualizar perfil funcionando sem a de imagem de perfil
    
    * fix: session['id'] por session['id_usuario']

--- backend/router/userRouter.php
+++ backend/router/userRouter.php
@@ -11,7 +11,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
                 $resultado = $userController->CriarUsuario($_POST["nome"],$_POST["email"],$_POST["senha"],$_POST["telefone"]);
                 if($resultado){
                     session_start();
-                    $_SESSION['id'] = $resultado['id'];
+                    $_SESSION['id_usuario'] = $resultado['id'];
                     header("location: ../../src/pages/home/index.php");
                 }else{
                     header("location: ../../src/pages/cadastro/index.php?error=true");
@@ -21,16 +21,31 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
             }
             break;
         
-        case "edit":
+        case "editar":
             if(!(empty($_POST['nome']) || empty($_POST['email']) || empty($_POST['senha']) || empty($_POST['telefone']))){
-                $resultado = $userController->AtualizarUsuario($_POST["id"], $_POST["nome"],$_POST["email"],$_POST["senha"],$_POST["telefone"]);
+                // $foto_perfil = null;
+                // if (!empty($_FILES['foto_perfil']['fotoPerfil'])){
+                //     $dir = __DIR__ . "/../../public/uploads/";
+                //     $file = $dir . basename($_FILES["foto_perfil"]["fotoPerfil"]);
+                //     if (move_uploaded_file($_FILES["foto_perfil"]["tmp_name"], $file)){
+                //         $foto_perfil = $file;
+                //     } else {
+                //         echo "Erro ao carregar.";
+                //         exit();
+                //     }
+                // }
+                $resultado = $userController->AtualizarUsuario($_POST["id"], $_POST["nome"],$_POST["email"],$_POST["senha"],$_POST["telefone"]); ##colocar $foto_perfil
                 if($resultado){
-                    header("location: ../../pages/home/index.php");
+                    header("location: ../../src/pages/perfil/perfil.php");
                 }else{
-                    header("location: ../../pages/signUp/index.php?error=true");
+                    echo "Erro ao atualizar o usuario";
+                    exit();
+                    // header("location: ../../src/pages/perfil/perfil.php?error=true");
                 }
             }else{
-                header("location: ../../pages/signUp/index.php?error=true");
+                echo "Campos obrigatorios estao faltando.";
+                exit();
+                // header("location: ../../src/pages/perfil/perfil.php?error=true");
             }
             break;
         
