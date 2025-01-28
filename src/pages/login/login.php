a113337583df5c296729c52e4452644904297f75
João Augusto Cintra Campos <joaoaugustocintracampos@gmail.com>
2025-01-28 16:42:10 -0400

    Feat/editar reserva (#16)
    * update login php
    
    * feat: page de editar reserva

--- src/pages/login/login.php
+++ src/pages/login/login.php
@@ -30,3 +30,86 @@
     </div>
 </body>
 </html>
+<style>
+
+*{
+    margin: 0;
+    padding: 0;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+body{
+    background-color: black;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.form-container {
+    height: 750px;
+    display: flex;
+    flex: 1;
+    justify-content: center;
+}
+
+.form-container form{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 3rem;
+    background-color: #1E1E1E;
+    border-radius: 0.5rem;
+
+}   
+
+img{
+    width: 52rem;
+}
+
+.campo-cadastro{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 0.1rem gray solid;
+    width: 70%;
+}
+
+.botoes-cadastro{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1.5rem;
+}
+form img{
+    width: 6rem;
+    margin-bottom: 4rem ;
+}
+.btn{
+    padding-left: 0.5rem;
+    border: 0.1rem #9CA3AF solid;
+    border-radius: 0.5rem;
+    width: 20.5rem;
+    height: 2.8rem;
+    font-size: medium;
+}
+
+.btn::placeholder{
+    color: #CED4DA;
+}
+
+.btn:nth-child(3){
+    background-color:  #13C296;
+    margin-top: 2rem;
+    width: 21.2rem;
+    height: 3.1rem;
+    color: white;
+    cursor: pointer;
+}
+.label{
+    display: block;
+    color: white;
+    margin-bottom: 0.8rem;
+}
+</style>
